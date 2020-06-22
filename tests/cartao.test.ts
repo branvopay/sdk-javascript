1777af2bf4de735341d7b437f81db0ab80db1bce
Richard Lopes <contato.richardlopes@outlook.com>
2020-06-21 21:16:48 -0300

    test: get transaction

--- tests/cartao.test.ts
+++ tests/cartao.test.ts
@@ -118,3 +118,18 @@ test('should make partial cancel', async () => {
 
   expect(response.success).toBe(true);
 });
+
+test.only('should consult transaction', async () => {
+  const config = new Configuration({
+    token: '23B5E1AE45EE03313AD3C3EC0B083707',
+    env: Environment.SANDBOX,
+  });
+
+  const cartao = new Cartao(config);
+
+  const orderNumber = (await cartao.transaction(payload())).data.orderNumber;
+
+  const response = await cartao.get(orderNumber);
+
+  expect(response.success).toBe(true);
+});
