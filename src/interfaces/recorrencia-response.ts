1e04e70e2edb297d6ff08691390adc6bd6fe14c1
Richard Lopes <contato.richardlopes@outlook.com>
2020-06-27 22:37:15 -0300

    fix: recurrence response

--- src/interfaces/recorrencia-response.ts
+++ src/interfaces/recorrencia-response.ts
@@ -1,13 +1,15 @@
 export interface RecorrenciaResponse {
-  active: boolean;
-  amount: number;
-  name: string;
-  cardDate: string;
-  cardNumber: string;
-  code: number;
-  createdAt: string;
-  lastCharge: string;
-  nextCharge: string;
-  finalDate: string;
-  frequence: string;
+  recurrence: {
+    active: boolean;
+    amount: number;
+    name: string;
+    cardDate: string;
+    cardNumber: string;
+    code: number;
+    createdAt: string;
+    lastCharge: string;
+    nextCharge: string;
+    finalDate: string;
+    frequence: string;
+  };
 }
